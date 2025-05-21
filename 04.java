86ba0a7073e3425348e61095d0f33de09433ac5a
Tharinda-Kelum <kelumtharinda3@gmail.com>
2025-05-22 01:29:18 +0530

    Update 04.java
    fixed all issues and added the name and ticket nos

--- 04.java
+++ 04.java
@@ -2,21 +2,11 @@ class Ticket{
 	int ticketNumber;
 	String customerName;
 	int seatnumber;
-	int[] bookedseat=new int[10];
-	int[] canceledseat=new int[10];
-	int no_of_seatsbooked=0;
-	int i=1;
-	int canceledseats=0;
-	int b=0;
 	
 	Ticket(int ticketNumber,String customerName,int seatnumber){
 		this.ticketNumber=ticketNumber;
 		this.customerName=customerName;
 		this.seatnumber=seatnumber;
-	}
-	
-	Ticket(){
-		
 	}
 	
 	int getTicketNumber(){
@@ -31,12 +21,38 @@ class Ticket{
 		return seatnumber;
 	}
 	
-	public void booking(){
+	public void displayDetails(){
+		System.out.println(ticketNumber+"\t\t"+customerName+"\t\t"+seatnumber);
+	}
+}
+
+class BookingSystem{
+	int[] bookedseat=new int[10];
+	int[] canceledseat=new int[10];
+	private Ticket[] tickets;
+	int no_of_seatsbooked;
+	int i=1;
+	int canceledseats=0;
+	int b=0;
+	
+	public BookingSystem(){
+		this.tickets= new Ticket[10];
+		this.no_of_seatsbooked=0;
+	}
+	
+	
+	public int booking(){
+		bookedseat[no_of_seatsbooked]=i;
+		no_of_seatsbooked=no_of_seatsbooked+1;
+		i++;
+		return i-1;
+	}
+	
+	public void bookingseat(Ticket ticket){
 		if(canceledseats==0){
 			if(no_of_seatsbooked<10){
-				bookedseat[no_of_seatsbooked]=i;
-				no_of_seatsbooked=no_of_seatsbooked+1;
-				i++;
+				tickets[no_of_seatsbooked-1]=ticket;
+				System.out.println("Your booking is successful and your seat no is "+bookedseat[no_of_seatsbooked-1]);
 			}
 			else{
 				System.out.println("Housefull no more tickets available.");
@@ -44,17 +60,11 @@ class Ticket{
 		}
 		else{
 			for(int c=0;c<canceledseats;c++){
-				System.out.println("Seat no "+canceledseat[c]+" is available.");
-			}
-		}
-	}
-	
-	public void booking(int seatnumber){
-		for(int c=0;c<canceledseats;c++){
-			System.out.println("a");
-			if(canceledseat[c]==seatnumber){
-				bookedseat[no_of_seatsbooked-1]=seatnumber;
-				System.out.println("b");
+				no_of_seatsbooked=no_of_seatsbooked+1;
+				if(canceledseat[c]==ticket.getSeatNumber()){
+					bookedseat[no_of_seatsbooked-1]=ticket.getSeatNumber();
+					tickets[no_of_seatsbooked-1]=ticket;
+				}
 			}
 		}
 	}
@@ -64,39 +74,43 @@ class Ticket{
 			if(bookedseat[j]==seatnumber){
 				canceledseats=canceledseats+1;
 				canceledseat[b]=seatnumber;
+				System.out.println("\nYour booking has been canceled for seat no "+seatnumber);
 				int a=j+1;
 				while(j<no_of_seatsbooked && a<no_of_seatsbooked){
-					bookedseat[j]=bookedseat[a];
+					tickets[j]=tickets[a];
 					j++;
 					a++;
 				}
-				bookedseat[no_of_seatsbooked-1]=0;
-				b++;
+				tickets[no_of_seatsbooked-1]=null;
+				no_of_seatsbooked=no_of_seatsbooked-1;
 			}
 		}
 	}
 	
-	public void display(){
-		for(int j=0;j<no_of_seatsbooked;j++){
-			if(bookedseat[j]!=0){
-				System.out.println(bookedseat[j]);
+	public void displayAllDetails(){
+		if(no_of_seatsbooked==0){
+			System.out.println("No booking available.");
+		}
+		else{
+			System.out.println("\nBooking details ");
+			for(int d=0;d<no_of_seatsbooked;d++){
+				tickets[d].displayDetails();
 			}
 		}
 	}
-	
-	
 }
 
 class Main{
 	public static void main(String[] args){
-		Ticket t1=new Ticket();
-		t1.booking();
-		t1.booking();
-		t1.booking();
-		t1.display();
-		t1.cancelticket(2);
-		t1.display();
-		t1.booking(2);
-		t1.display();
+		int c=0;
+		BookingSystem b1=new BookingSystem();
+		b1.bookingseat(new Ticket(++c,"Alice",b1.booking()));
+		b1.bookingseat(new Ticket(++c,"Bob",b1.booking()));
+		b1.bookingseat(new Ticket(++c,"Namal",b1.booking()));
+		b1.displayAllDetails();
+		b1.cancelticket(2);
+		b1.displayAllDetails();
+		b1.bookingseat(new Ticket(++c,"Bob",2));
+		b1.displayAllDetails();
 	}
 }
